ab437ad12328e921ef9a9545ac459e6464774ec1
Mithun <mithun.vikram@10decoders.in>
2021-02-01 12:47:26 +0530

    camunda url changes

--- testticketone_3611/application/client/desktop/testticketone/src/app/config/Constant.ts
+++ testticketone_3611/application/client/desktop/testticketone/src/app/config/Constant.ts
@@ -5,10 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class Constants {
 
-    public pageTitles: string = 'http://localhost:8002/dmngenerate';
 
-    public savescreen: string = 'http://localhost:8002/savescreen';
+    public pageTitles: string = 'http://'+window.location.hostname+':8002/dmngenerate';
 
-    public getallscreen: string = 'http://localhost:8002/getallscreens';
+    public savescreen: string = 'http://'+window.location.hostname+':8002/savescreen';
+
+    public getallscreen: string = 'http://'+window.location.hostname+':8002/getallscreens';
 
 }
